0e41b37fd13e59048d92a3b5c3fe117c8345f336
ashishaman13 <ashish34gupta@gmail.com>
2026-03-21 07:40:22 +0530

    creating identical resources with count

---
+++ Certification-labs/count/ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "tf-user-personal-account"
+}
+
+resource "aws_instance" "my_ec2" {
+    ami = "ami-1234567890"
+    instance_type = "t2.micro"
+    count = 3
+    tags = {
+        Name = "order-instance"
+    }
+}
