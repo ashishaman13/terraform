787aa2e05e5cef9cb33e955c0e12e1b88bfe6189
ashishaman13 <ashish34gupta@gmail.com>
2026-04-25 09:05:12 +0530

    learnt conditional expression

--- Certification-labs/conditional-expression/conditional-expression.tf
+++ Certification-labs/conditional-expression/conditional-expression.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    region = "us-east-1"
-    profile = "tf-user-personal-account"
-  
+  region  = "us-east-1"
+  profile = "tf-user-personal-account"
+
 }
 
 variable "environment" {
@@ -15,31 +15,31 @@ resource "aws_instance" "my_ec2" {
 
 # Using not EQUALS
 resource "aws_instance" "my_ec2" {
-  ami = "ami-098e39bafa7e7303d"
+  ami           = "ami-098e39bafa7e7303d"
   instance_type = var.environment != "devlopment" ? "t2.micro" : "m5.large"
 }
 
 # Empty value based example
 variable "env" {
-    default = ""
+  default = ""
 }
 
 resource "aws_instance" "name" {
-  ami = "ami-098e39bafa7e7303d"
+  ami           = "ami-098e39bafa7e7303d"
   instance_type = var.env == "" ? "t2.micro" : "m5.xlarge"
 }
 
 # With Multiple Variable
 variable "environment" {
-    default = ""
-  
+  default = ""
+
 }
 
 variable "region" {
-    default = "us-east-1"
+  default = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-    ami = "ami-098e39bafa7e7303d"
-    instance_type = var.environment == "production" && var.region == "us-east-1" ? "m5.large" : "t2.micro"
+  ami           = "ami-098e39bafa7e7303d"
+  instance_type = var.environment == "production" && var.region == "us-east-1" ? "m5.large" : "t2.micro"
 }
