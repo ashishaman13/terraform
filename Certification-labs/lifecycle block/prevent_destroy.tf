c9457f97a03cc0072c17dbbf1ee73d306ce80899
ashishaman13 <ashish34gupta@gmail.com>
2026-04-27 12:05:23 +0530

    learnt lifecycle block

---
+++ Certification-labs/lifecycle block/prevent_destroy.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~>1.14.9"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>6.42"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "tf-user-personal-account"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0ec10929233384c7f"
+    instance_type = "t3.micro"
+
+    tags = {
+      "Name" = "HelloEarth_ec2"
+    }
+
+    lifecycle {
+      prevent_destroy = true
+    }
+}
